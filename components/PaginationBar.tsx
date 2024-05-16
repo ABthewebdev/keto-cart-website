29635dc96c0a38f1a12a8f1f579021b51fa10a92
Alex Brewster <alexgbrewster@gmail.com>
2024-05-15 23:59:23 -0400

    pagination component

---
+++ components/PaginationBar.tsx
@@ -0,0 +1,56 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+}
+
+export default function PaginationBar({
+  currentPage,
+  totalPages,
+}: PaginationProps) {
+  const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 10));
+  const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
+  const numberedPageItems: JSX.Element[] = [];
+  for (let page = minPage; page <= maxPage; page++) {
+    numberedPageItems.push(
+      <PaginationItem key={page} className={``}>
+        <PaginationLink href={"?page=" + page}>{page}</PaginationLink>
+      </PaginationItem>
+    );
+  }
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
